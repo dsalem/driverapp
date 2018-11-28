54c58608d4f82db2f4e7127411791c45a44c9f85
David Salem <dsalem9191@gmail.com>
2018-11-28 10:49:33 +0200

    driver and ride classes

--- app/src/main/java/model/model/entities/Driver.java
+++ app/src/main/java/model/model/entities/Driver.java
@@ -1,4 +1,127 @@
 package model.model.entities;
 
 public class Driver {
+    /**
+     * fields
+     */
+    private String lastName;
+    private String firstName;
+    private Long id;
+    private Long phoneNumber;
+    private String emailAddress;
+    private Long creditCard;
+
+    /**
+     *
+     * @param lastName
+     * @param firstName
+     * @param id
+     * @param phoneNumber
+     * @param emailAddress
+     * @param creditCard
+     */
+    public Driver(String lastName, String firstName, Long id, Long phoneNumber, String emailAddress, Long creditCard) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.creditCard = creditCard;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     *
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     *
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     *
+     * @param phoneNumber
+     */
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /**
+     *
+     * @param emailAddress
+     */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Long getCreditCard() {
+        return creditCard;
+    }
+
+    /**
+     *
+     * @param creditCard
+     */
+    public void setCreditCard(Long creditCard) {
+        this.creditCard = creditCard;
+    }
 }
---
+++ app/src/main/java/model/model/entities/Ride.java
@@ -0,0 +1,80 @@
+package model.model.entities;
+
+import java.sql.Time;
+
+public class Ride {
+    public enum Status {WAITING,HANDLING,CLOSED}
+    private String startLocation;
+    private String destinationLocation;
+    private Time startTime;
+    private Time endTime;
+    private String passengerName;
+    private Long passengerPhoneNumber;
+    private String passengerEmail;
+
+    public Ride(String startLocation, String destinationLocation, Time startTime, Time endTime, String passengerName, Long passengerPhoneNumber, String passengerEmail) {
+        this.startLocation = startLocation;
+        this.destinationLocation = destinationLocation;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.passengerName = passengerName;
+        this.passengerPhoneNumber = passengerPhoneNumber;
+        this.passengerEmail = passengerEmail;
+    }
+
+    public String getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(String startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public String getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public void setDestinationLocation(String destinationLocation) {
+        this.destinationLocation = destinationLocation;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public Long getPassengerPhoneNumber() {
+        return passengerPhoneNumber;
+    }
+
+    public void setPassengerPhoneNumber(Long passengerPhoneNumber) {
+        this.passengerPhoneNumber = passengerPhoneNumber;
+    }
+
+    public String getPassengerEmail() {
+        return passengerEmail;
+    }
+
+    public void setPassengerEmail(String passengerEmail) {
+        this.passengerEmail = passengerEmail;
+    }
+}
